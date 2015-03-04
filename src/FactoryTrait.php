21d80bb03f23449e55aca0b90cc3265f644eb039
Thomas ZILLIOX <thomas@zilliox.me>
2015-03-04 15:23:41 +0100

    Add  error handler

--- src/FactoryTrait.php
+++ src/FactoryTrait.php
@@ -17,37 +17,69 @@ trait FactoryTrait
         if (is_object($className)) {
             $className = get_class($className);
         }
-        $factoryName = get_called_class();
-        if (!isset(self::$indexList[$factoryName])) {
-            self::$indexList[$factoryName] = [];
-        }
         if (!is_array($indexList)) {
             $indexList = [$indexList];
         }
         foreach ($indexList as $index) {
-            $id = strtolower($index);
-            self::$indexList[$factoryName][$id] = $className;
+            $index = strtolower($index);
+            if ($this->hasIndex($index)) {
+                $this->onClassIndexOverride($index, $className);
+            }
+            $this->setIndex($index, $className);
         }
         return $this;
     }
 
+    public function retrieveClass($index)
+    {
+        $index = strtolower($index);
+        if (!$this->hasIndex($index)) {
+            return $this->onNoClassIndexFound($index);
+        }
+        return $this->getIndex($index);
+    }
+
     public function newInstance($index)
     {
-        $factoryName = get_called_class();
-        if (!isset(self::$indexList[$factoryName])) {
-            return $this->onNoClassIndexFound($index);
-        }
-        $id = strtolower($index);
-        if (!isset(self::$indexList[$factoryName][$id])) {
-            return $this->onNoClassIndexFound($index);
-        }
-        $className = self::$indexList[$factoryName][$id];
+        $className = $this->retrieveClass($index);
         return new $className;
     }
 
 
-    /* PROTECTED METHODS
+    /* PRIVATE INDEX METHODS
      *************************************************************************/
+    private function setIndex($index, $className)
+    {
+        $factoryName = get_called_class();
+        if (!isset(self::$indexList[$factoryName])) {
+            self::$indexList[$factoryName] = [];
+        }
+        self::$indexList[$factoryName][$index] = $className;
+    }
+
+    private function hasIndex($index)
+    {
+        $factoryName = get_called_class();
+        if (!isset(self::$indexList[$factoryName])) {
+            return false;
+        }
+        return isset(self::$indexList[$factoryName][$index]);
+    }
+
+    private function getIndex($index)
+    {
+        $factoryName = get_called_class();
+        return self::$indexList[$factoryName][$index];
+    }
+
+
+    /* PROTECTED ERROR METHODS
+     *************************************************************************/
+    protected function onClassIndexOverride($index, $className)
+    {
+        throw new \RuntimeException('Class already registered for the index: ' . $index);
+    }
+    
     protected function onNoClassIndexFound($index)
     {
         throw new \RuntimeException('No class registered for the index: ' . $index);
